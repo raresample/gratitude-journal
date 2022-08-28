a25dc44c72ad8fd8c4fe67ca652c556184465fbd
Aaron Hope <aghopecode@gmail.com>
2022-08-28 02:36:15 -0700

    Disabled grid on GratitudeCard div.card to improve alignment

--- src/components/GratitudeCard.vue
+++ src/components/GratitudeCard.vue
@@ -12,6 +12,7 @@
           {{ displayDate }}
         </div>
       </div>
+
       <div class="delete">
         <div class="like">
           <div class="likes">
@@ -58,8 +59,8 @@ export default {
   /* box-shadow: 0 0 3px rgba(0,0,0,0.25); */
   margin: 1rem 0;
   padding: 1.5rem;
-  display: grid;
-  grid-template-rows: repeat(2, minmax(0, 1fr));
+  /* display: grid;
+  grid-template-rows: repeat(2, minmax(0, 1fr)); */
   min-height: 10rem;
   align-items: stretch;
   box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 20%), 0 1px 5px 0 rgb(0 0 0 / 12%);
